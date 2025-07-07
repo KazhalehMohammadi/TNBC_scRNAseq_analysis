abe41e106fb9e5b1154f523b32867e70a0b7856a
KazhalehMohammadi <112824716+KazhalehMohammadi@users.noreply.github.com>
2025-07-07 20:10:21 +0300

    Update Module-Mem-Pacli-B.R

--- Module-Mem-Pacli-B.R
+++ Module-Mem-Pacli-B.R
@@ -3,118 +3,162 @@ library(Matrix)
 library(WGCNA)
 options(stringsAsFactors = FALSE)
 allowWGCNAThreads()
-# #------------------------------read blood, paclitaxel plus atezolizumab-------------------------------------
-# folder_names <- c("Post_P001_b","Post_P005_b","Post_P0012_b","Post_P0014_b","Post_P0017_b","Post_P0019_b",
-#                   "Pre_P001_b","Pre_P002_b","Pre_P004_b","Pre_P005_b","Pre_P007_b","Pre_P0010_b",
-#                   "Pre_P0012_b","Pre_P0014_b","Pre_P0016_b","Pre_P0017_b","Pre_P0019_b",
-#                   "Prog_P002_b","Prog_P004_b","Prog_P007_b","Prog_P0010_b","Prog_P0016_b")  
-#------------------------------read blood, paclitaxel-------------------------------------
- folder_names <- c("Post_P011_b","Post_P018_b","Post_P020_b","Post_P022_b", "Post_P023_b","Post_P024_b","Post_P025_b",
+
+# ------------------------------ Read blood, paclitaxel plus atezolizumab -------------------------------------
+# List of sample names for blood samples treated with paclitaxel plus atezolizumab
+folder_names <- c("Post_P011_b","Post_P018_b","Post_P020_b","Post_P022_b", "Post_P023_b","Post_P024_b","Post_P025_b",
                    "Pre_P008_b","Pre_P011_b","Pre_P013_b","Pre_P018_b","Pre_P020_b","Pre_P022_b","Pre_P023_b","Pre_P024_b","Pre_P025_b",
                    "Prog_P008_b","Prog_P013_b")  
-#------------------------------read tumor, paclitaxel plus atezolizumab-------------------------------------
+
+# ------------------------------ Read tumor, paclitaxel plus atezolizumab -------------------------------------
+# The following sample set (commented) refers to tumor samples treated with paclitaxel plus atezolizumab.
 # folder_names <- c("Post_P002_t","Pre_P004_t","Post_P005_t","Post_P012_t","Post_P016_t","Post_P017_t","Post_P019_t",
 #                   "Pre_P002_t","Pre_P005_t","Pre_P007_t","Pre_P012_t","Pre_P016_t","Pre_P017_t","Pre_P019_t",
 #                   "Prog_P007_t")  
-# #------------------------------read tumor, paclitaxel-------------------------------------
-# folder_names <- c("Post_P003_t","Post_P0018_t","Post_P0020_t","Post_P0022_t","Post_P0023_t","Post_P0024_t","Post_P0025_t",
-#                   "Pre_P0013_t","Pre_P0018_t","Pre_P0020_t","Pre_P0022_t","Pre_P0023_t","Pre_P0025_t",
-#                   "Prog_P0013_t")  
-#------------------------------------------------------------------------------------
+
+# ------------------------------ Read tumor, paclitaxel -------------------------------------
+# A different set of tumor samples treated with paclitaxel (this is being used in this case)
+folder_names <- c("Post_P003_t","Post_P018_t","Post_P020_t","Post_P022_t","Post_P023_t","Post_P025_t",
+                   "Pre_P013_t","Pre_P018_t","Pre_P020_t","Pre_P022_t","Pre_P023_t","Pre_P025_t",
+                   "Prog_P013_t")  
+
+# ------------------------------------------------------------------------------------
 seurat_list <- list()
 for (i in seq_along(folder_names)) {
   folder_name <- folder_names[i]
   sample_path <- file.path("D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis", folder_name)
+  
+  # Reading the data using the 10X Genomics format
   data <- Read10X(data.dir = sample_path)
   seurat_object <- CreateSeuratObject(counts = data, min.features = 100, project = folder_name)
+  
+  # Add metadata to identify the sample origin and its condition (pre, post, or progressive)
   seurat_object$orig.ident <- folder_name
   seurat_object$sample <- tolower(strsplit(folder_name, "_")[[1]][1])
+  
+  # Store Seurat object in a list
   seurat_list[[folder_name]] <- seurat_object
 }
-#---------merge
+
+# --------- Merge the Seurat objects from different samples
 merged_samples <- merge(
-  x = seurat_list[[1]],
-  y = seurat_list[-1],
-  add.cell.ids = names(seurat_list),   # Take the folder names
-  project = "Blood_samples"
+  x = seurat_list[[1]],  # Start with the first Seurat object
+  y = seurat_list[-1],   # Merge the rest of the Seurat objects in the list
+  add.cell.ids = names(seurat_list),  # Use the folder names as unique cell IDs
+  project = "Tumor_samples"  # Assign a project name to the merged object
 )
 
+# Create a list of sample identifiers, and assign trait values ("1" for post and "2" for progressive samples)
 samples <- folder_names
 post_samples <- samples[grepl("Post|Prog", samples)]
 trait <- ifelse(grepl("Prog", post_samples), "2", "1")
 
+# Remove unnecessary variables to save memory
 rm(seurat_list)
 rm(seurat_object)
 rm(data)
-#---------pseudo bulk
-raw_counts <- GetAssayData(merged_samples, layer = "counts")
-head(raw_counts)
-dim(raw_counts)            # gene number* cell number
-cell_counts <- table(merged_samples$orig.ident) #number of cell
+
+# --------- Pseudo bulk transformation: Aggregating gene counts across cells by sample
+raw_counts <- GetAssayData(merged_samples, layer = "counts")  # Extract the raw count data
+head(raw_counts)  # Preview the data
+dim(raw_counts)   # Display the dimensions (genes x cells)
+
+# Count the number of cells per sample
+cell_counts <- table(merged_samples$orig.ident)
 cell_counts
+
+# Aggregate the counts across all cells in each sample (pre/post-treatment groups)
 pseudo_bulk <- t(rowsum(t(as.matrix(raw_counts)), group = merged_samples$orig.ident))
 head(pseudo_bulk)
-dim(pseudo_bulk)   # gene number* Sample number
+dim(pseudo_bulk)  # Check the new dimensions (genes x samples)
+
+# Normalize by the number of cells in each sample
 mean_counts <- sweep(pseudo_bulk, 2, cell_counts, FUN = "/")
 head(mean_counts)
 dim(mean_counts)
-pseudo_bulk<-mean_counts
+
+# Update pseudo_bulk with the normalized counts
+pseudo_bulk <- mean_counts
 rm(mean_counts)
 rm(raw_counts)
 rm(combined)
-#----------split samples based on pre and post for construct two wgcna network
-sample_names <- colnames(pseudo_bulk)
 
-sample_group <- ifelse(grepl("^Pre", sample_names), "pre", "post")
+# --------- Split samples into pre-treatment and post-treatment groups for constructing two WGCNA networks
+sample_names <- colnames(pseudo_bulk)
+sample_group <- ifelse(grepl("^Pre", sample_names), "pre", "post")  # Categorize samples as "pre" or "post"
 table(sample_group)
 
-pre_samples <-  colnames(pseudo_bulk)[sample_group == "pre"]
+pre_samples <- colnames(pseudo_bulk)[sample_group == "pre"]
 post_samples <- colnames(pseudo_bulk)[sample_group == "post"]
 
+# Create expression matrices for pre and post treatment
 datExpr_pre <- as.data.frame(t(pseudo_bulk[, pre_samples]))
 datExpr_post <- as.data.frame(t(pseudo_bulk[, post_samples]))
-#----------
+
+# ---------- Variance filtering: Remove genes with zero variance across both pre and post groups
 var_pre  <- apply(datExpr_pre, 2, var)
 var_post <- apply(datExpr_post, 2, var)
+
+# Identify genes that have zero variance in both pre and post groups
 common_zero_var_genes <- names(var_pre)[var_pre == 0 & var_post == 0]
-datExpr_pre  <- datExpr_pre[, !colnames(datExpr_pre) %in% common_zero_var_genes]
-View(datExpr_pre)
-datExpr_post <- datExpr_post[, !colnames(datExpr_post) %in% common_zero_var_genes]
-#---------------------------
-data1 <- read.csv("D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis\\TOP10-Clusters-final-Blood-Pacli.csv")
-#data2 <- read.csv("D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\TOP10-Clusters-final-Blood-Pacli.csv")
+datExpr_pre  <- datExpr_pre[, !colnames(datExpr_pre) %in% common_zero_var_genes]  # Remove those genes from pre-treatment data
+View(datExpr_pre)  # Check the filtered data
+datExpr_post <- datExpr_post[, !colnames(datExpr_post) %in% common_zero_var_genes]  # Apply the same filtering to post-treatment data
+
+# ---------------------------
+data1 <- read.csv("D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis\\TOP10-Clusters-final-Blood-Pacli.csv")  # Read gene information from CSV
+# data2 <- read.csv("D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\TOP10-Clusters-final-Blood-Pacli.csv")  # Uncomment if necessary
+
+# Create an output directory if it doesn't already exist
 output_dir <- "D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis\\Results"
-dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)  # ایجاد مسیر در صورت عدم وجود
+dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+
+# Extract gene information (gene names)
 gene_info <- data1[, c("gene")]
 head(gene_info)
+
+# Select genes that are present in both the gene information list and the pre-treatment data
 genes_to_use <- intersect(gene_info, colnames(datExpr_pre))
 datExpr_pre1 <- datExpr_pre[, genes_to_use]
 
+# Get the number of modules in the pre-treatment network
 num_modules <- ncol(MEs_pr)
 head(MEs_pr)
-module_names <- colnames(MEs_pr)
+module_names <- colnames(MEs_pr)  # Get the names of the modules
 module_names
-genes_to_use <- intersect(gene_info, colnames(datExpr_pre))
-datExpr_pre1 <- datExpr_pre[, genes_to_use]
 
+# Common Gene Selection
+genes_to_use <- intersect(gene_info, colnames(datExpr_pre))  # Select genes that are common in both the data and gene info
+datExpr_pre1 <- datExpr_pre[, genes_to_use]  # Subset the data to include only those genes
+
+# Eigen Gene Calculation: Matching samples between eigengenes and selected genes
 common_samples <- intersect(rownames(MEs_pr), rownames(datExpr_pre1))
-MEs_mat <- MEs_pr[common_samples, ]
-expr_mat <- datExpr_pre1[common_samples, ]
-length(common_samples)
-head(rownames(MEs_pr))
-head(rownames(datExpr_pre1))
+MEs_mat <- MEs_pr[common_samples, ]  # Subset the eigengene matrix
+expr_mat <- datExpr_pre1[common_samples, ]  # Subset the gene expression matrix
 
-# Module membership (kME)
+length(common_samples)  # Check the number of common samples
+head(rownames(MEs_pr))  # Preview row names (samples) of eigengenes
+head(rownames(datExpr_pre1))  # Preview row names (samples) of the expression matrix
+
+# Module membership (kME): Correlation between gene expression and module eigengenes
 module_membership <- cor(expr_mat, MEs_mat, use = "pairwise.complete.obs")
+
+# Save module membership to a CSV file
 write.csv(module_membership, file = "D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis\\Results\\ModuleMembership_NewData.csv")
+
+# Weighted correlation matrix: Multiply module membership by module importance
 weighted_cor_matrix <- module_membership * module_importance  
+
+# Save the weighted correlation matrix to a CSV file
 write.csv(weighted_cor_matrix, file = "D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis\\Results\\Weighted_ModuleCorrelation_Pre_vs_Post.csv")
+
+# Find the maximum membership for each gene and save to CSV
 max_membership_per_gene <- apply(weighted_cor_matrix, 1, max)
 
 max_membership_df <- data.frame(
   gene = rownames(weighted_cor_matrix),
   Max_Membership = max_membership_per_gene
 )
+
+# Save the maximum module membership per gene to a CSV file
 write.csv(max_membership_df, file = "D:\\Thesis\\Proposal\\Analysis\\WGCNA\\Blood-Patient\\Blood-Patient-Paclitaxel-Analysis\\Results\\Max_Module_Membership_Per_Gene.csv", row.names = FALSE)
-
-
